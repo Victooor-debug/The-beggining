844140f8664d64dcd295de152655073b0692f71a
Victooor-debug <20242431@itla.edu.do>
2026-06-08 21:42:31 -0400

    Add files via upload

---
+++ PruebaCalculadoraMatematica.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author benya
+ */
+public class PruebaCalculadoraMatematica {
+    
+     public static void main(String[] args) {
+        
+     
+    
+     CalculadoraMatematica calc = new CalculadoraMatematica();
+
+        calc.menu();
+    }
+
+}
